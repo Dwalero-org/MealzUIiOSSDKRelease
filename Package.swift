50d3da1c1b0e5dce837b630539369e454f1051a6
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-07 16:22:27 +0200

    update release

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let configurationMode = "prod"
+
+let package = Package(
+    name: "MealzUIiOSSDK",
+    defaultLocalization: "fr",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "MealzUIiOSSDK",
+            targets: ["MealzUIiOSSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Dwalero-org/MealzIOSFrameworkRelease", from: "1.0.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MealzUIiOSSDK",
+            url: "https://github.com/Dwalero-org/MealzUIiOSSDKRelease/raw/release/1.0.0/MealzUIiOSSDK.zip",
+            checksum: "2b74a4fdaaba3f6696aca7345d584bfd83fea35ac85eb97cb5e9e294f2f26d80"
+        )
+        
+    ]
+)
+/*
+ 
+ */
