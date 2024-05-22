46c00354f134c1a7d0da12262ee4f0d7191fc210
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-22 13:24:27 +0200

    update release

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
             targets: ["MealzUIiOSSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Dwalero-org/MealzIOSFrameworkRelease", from: "1.0.0")
+        .package(url: "https://github.com/miamtech/MealzIOSFrameworkRelease", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "MealzUIiOSSDK",
-            url: "https://github.com/Dwalero-org/MealzUIiOSSDKRelease/raw/release/1.0.0/MealzUIiOSSDK.zip",
-            checksum: "2b74a4fdaaba3f6696aca7345d584bfd83fea35ac85eb97cb5e9e294f2f26d80"
+            url: "https://github.com/Dwalero-org/MealzUIiOSSDKRelease/raw/release/1.0.2/MealzUIiOSSDK.zip",
+            checksum: "be79bc591f6b8091fcbc6ae8416e46e74659168447f1c08554842fa9fa8de880"
         )
         
     ]
